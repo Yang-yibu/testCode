f4e6609f2c5a35556a71f59c65496baffb5ab5c0
jrf <yyn@126mail.cn>
2018-09-06 17:05:48 +0800

    数组热更新

--- other/index.js
+++ other/index.js
@@ -9,6 +9,42 @@
         console.log(this)
         console.log(this.get)
     }
-    aa();
+    // aa();
+
+
+    // 数组热更新
+    function arrraySplice() {
+        let a = ['a', 'b', { a: 1, b: 2 }, 'c', 'd', 'f'];
+        let b = [{ c: 2, d: { a: 1 } }, { a: 1 }, '1'];
+
+        // 替换的起始位置（下标），替换的数组长度，替换的元素
+        let c = a.splice(5, b.length, ...b);
+
+        console.log(c);
+        console.log(a, c)
+    }
+    arrraySplice();
+
+    // js 循环
+    function switchLoop(styleName) {
+        switch (styleName) {
+            case 'text-align':
+                console.log('textAlign')
+                break;
+            case 'font-weight':
+                console.log('fontWeight')
+                break;
+            case 'font-size':
+                console.log('fontSize')
+                break;
+            case ('background-color'):
+                console.log('background')
+                break;
+            case 'color':
+                console.log('colorArr')
+                break;
+        }
+    }
+    switchLoop('background');
 }(window, document));
-console.log(aa)
+
